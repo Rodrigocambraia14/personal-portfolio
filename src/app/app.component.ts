a4e2c0b67d2c743ae6a4ba3960339f3b2c465b16
Rodrigo Gonçalves Cambraia Soares <rodrigocambraia.jr@gmail.com>
2022-03-13 02:43:48 -0300

    download de curriculo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-portfolio';
+  downloaded: boolean = false;
+
+  ngOnInit(): void {
+    
+  }
+
+  checkAge() {
+    var year = new Date().getFullYear();
+
+    var age = year - 1999;
+
+    if (new Date().getMonth() > 9)
+      return age;
+    else
+      return age - 1;
+  }
+
+  downloadPdf(url: any)
+  {
+    if (this.downloaded)
+    {
+      this.downloaded = false;
+      return;
+    }
+    
+      const a = document.createElement('a')
+      a.href = url
+      a.download = url.split('/').pop()
+      document.body.appendChild(a)
+      a.click()
+      document.body.removeChild(a)
+      this.downloaded = true;
+  }
 }
