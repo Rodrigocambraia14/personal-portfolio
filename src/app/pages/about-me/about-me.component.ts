7856748e2919628a386181018c5da841bb17c6f9
Rodrigo Gonçalves Cambraia Soares <rodrigocambraia.jr@gmail.com>
2022-03-16 01:34:31 -0300

    about-me parcial

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit{
+  devText: boolean = true;
+  personText: boolean = false;
 
   ngOnInit(): void {
     
   }
 
-
+  changeTextView(type: string)
+  {
+    if (type == 'developer')
+    {
+      this.devText = true;
+      this.personText = false;
+    }
+    else{
+      this.personText = true;
+      this.devText = false;
+    }
+  }
 }
